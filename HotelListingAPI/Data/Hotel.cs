2beac947935e0df58df1216229cf411a36ee47b8
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-05-04 00:33:28 +0100

    Added Entity Framework and created database

---
+++ HotelListingAPI/Data/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListingAPI.Data;
+
+public class Hotel
+{
+    public int Id { get; set; } // Primary Key (PK)
+    public string Name { get; set; }
+    public string Address { get; set; }
+
+    public double Rating { get; set; }
+
+    [ForeignKey(nameof(CountryId))]
+    public int CountryId { get; set; }  // Foreign Key (FK)
+
+    public Country Country { get; set; }
+}
